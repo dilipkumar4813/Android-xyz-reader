2f1e256c498bcf12dd4c162543798b32895fcf0d
Dilip <dilipkumar4813@gmail.com>
2017-06-07 10:02:14 +0530

    Tablet layout w600 and w900

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -12,10 +12,9 @@ public class Config {
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            url = new URL("https://go.udacity.com/xyz-reader-json");
         } catch (MalformedURLException ignored) {
-            // TODO: throw a real error
-            Log.e(TAG, "Please check your internet connection.");
+            Log.e(TAG, ignored.getLocalizedMessage());
         }
 
         BASE_URL = url;
